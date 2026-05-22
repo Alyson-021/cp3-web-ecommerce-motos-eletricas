3fc3c55588fc68ce98a3e415cf6956922602d83e
Alyson Souto <alysonartist@gmail.com>
2026-05-21 23:31:46 -0300

    feat: adicionando aba catalogo e estilizando no css

--- src/js/script.js
+++ src/js/script.js
@@ -194,85 +194,85 @@ if(listaCarrinho){
 
 
 
-// Array principal contendo os produtos do ecommerce
+// // Array principal contendo os produtos do ecommerce
 
-const produtos = [
-    {
-        // Objetos - {nome e preco} representando entidades
-        nome: "GCX X11",
-        preco: 8990,
-        imagem: "./src/assets/img/moto01.jpg",
-        perfil: "Explorador Urbano"
-    },
-    {
-        nome: "Shineray SE 1",
-        preco: 12990,
-        imagem: "./src/assets/img/moto02.jpg",
-        perfil: "Trabalhador Inteligente"
-    },
-    {
-        nome: "Yadea Owin",
-        preco: 15990,
-        imagem: "./src/assets/img/moto03.jpg",
-        perfil: "Lifestyle Premium"
-    },
-    {
-        nome: "Voltz EV1",
-        preco: 18990,
-        imagem: "./src/assets/img/moto04.jpg",
-        perfil: "Conectado Digital"
-    },
-    {
-        nome: "Watts W125",
-        preco: 22990,
-        imagem: "./src/assets/img/moto05.jpg",
-        perfil: "Motociclista Tradicional"
-    }
-];
+// const produtos = [
+//     {
+//         // Objetos - {nome e preco} representando entidades
+//         nome: "GCX X11",
+//         preco: 8990,
+//         imagem: "./src/assets/img/moto01.jpg",
+//         perfil: "Explorador Urbano"
+//     },
+//     {
+//         nome: "Shineray SE 1",
+//         preco: 12990,
+//         imagem: "./src/assets/img/moto02.jpg",
+//         perfil: "Trabalhador Inteligente"
+//     },
+//     {
+//         nome: "Yadea Owin",
+//         preco: 15990,
+//         imagem: "./src/assets/img/moto03.jpg",
+//         perfil: "Lifestyle Premium"
+//     },
+//     {
+//         nome: "Voltz EV1",
+//         preco: 18990,
+//         imagem: "./src/assets/img/moto04.jpg",
+//         perfil: "Conectado Digital"
+//     },
+//     {
+//         nome: "Watts W125",
+//         preco: 22990,
+//         imagem: "./src/assets/img/moto05.jpg",
+//         perfil: "Motociclista Tradicional"
+//     }
+// ];
 
-// ===============================
-// CAPTURANDO ELEMENTO DO HTML
-// ===============================
+// // ===============================
+// // CAPTURANDO ELEMENTO DO HTML
+// // ===============================
 
-// Local onde os cards serão criados / Manipulação em DOM
-const container = document.getElementById("container-produtos");
+// // Local onde os cards serão criados / Manipulação em DOM
+// const container = document.getElementById("container-produtos");
 
-// ===============================
-// Função para criar os Cards
-// ===============================
+// // ===============================
+// // Função para criar os Cards
+// // ===============================
 
-function renderizarProdutos() {
-    // Loop - Percorre todos os produtos/arrays
-    produtos.forEach(produto => {
+// function renderizarProdutos() {
+//     // Loop - Percorre todos os produtos/arrays
+//     produtos.forEach(produto => {
 
-        // Criando uma div
-        const card = document.createElement("div");
-        // Adicionando uma classe
-        card.classList.add("card");
+//         // Criando uma div
+//         const card = document.createElement("div");
+//         // Adicionando uma classe
+//         card.classList.add("card");
 
-        // Estrutura interna do Card
-        // Pegando o preço do produto e formatando no padrão brasileiro.
-        // <pan> Template String `texto ${variavel}`
-        card.innerHTML = `
-        <img src="${produto.imagem}" alt="${produto.nome}">
-        <h3>${produto.nome}</h3>
-        <p>${produto.perfil}</p>
-        <span>
-            R$ ${produto.preco.toLocaleString("pt-BR")}
-        </span>
+//         // Estrutura interna do Card
+//         // Pegando o preço do produto e formatando no padrão brasileiro.
+//         // <pan> Template String `texto ${variavel}`
+//         card.innerHTML = `
+//         <img src="${produto.imagem}" alt="${produto.nome}">
+//         <h3>${produto.nome}</h3>
+//         <p>${produto.perfil}</p>
+//         <span>
+//             R$ ${produto.preco.toLocaleString("pt-BR")}
+//         </span>
 
-        <button>
-            Comprar
-        </button>
+//         <button>
+//             Comprar
+//         </button>
         
-        `;
-        // Inserindo card no HTML
-        container.appendChild(card);
-    })
-}
+//         `;
+//         // Inserindo card no HTML
+//         container.appendChild(card);
+//     })
+// }
 
-// Executando a função
-renderizarProdutos();
+// // Executando a função
+// renderizarProdutos();
 
 
 
